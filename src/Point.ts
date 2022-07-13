ad45a5799aba754d436697113dc3c04cddd7cf97
dipesh-personal <dipbarbole@gmail.com>
2022-07-13 14:58:50 +0530

    methods upgraded

--- src/Point.ts
+++ src/Point.ts
@@ -2,6 +2,7 @@ export class Point {
   private x: number;
   private y: number;
 
+  constructor();
   constructor(x: number, y: number);
 
   constructor(x: number = 0, y: number = 0) {
@@ -13,9 +14,23 @@ export class Point {
     return `(${this.x}, ${this.y})`;
   }
 
-  distance(): number | Point;
-  distance(other: Point): number | Point;
-  distance(a?: any, b?: any): number | Point {
-    return Math.sqrt(Math.pow(a - this.x, 2) + Math.pow(b - this.y, 2));
+  distance(): number;
+  distance(other: Point): number;
+  distance(x: number, y: number): number;
+
+  distance(...args: any[]): number {
+    switch (typeof args[0]) {
+      case "undefined":
+        return this.distanceTo(0, 0);
+      case "object":
+        return this.distanceTo(args[0].x, args[0].y);
+      case "number":
+        return this.distanceTo(args[0], args[1]);
+    }
+  }
+
+  private distanceTo(x: number, y: number): number {
+    return +Math.sqrt((this.x - x) ** 2 + (this.y - y) ** 2).toFixed(3);
   }
 }
+
